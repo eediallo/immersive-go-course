884def1be2a5872dc7d978d30445ca152ab82887
eediallo <elhhabdouldiallo@gmail.com>
2025-09-01 13:34:44 +0100

    Implement directory reading and content display in Execute function

--- projects/cli-files/go-ls/cmd/root.go
+++ projects/cli-files/go-ls/cmd/root.go
@@ -1,3 +1,24 @@
 package cmd
 
-func Execute() {}
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func Execute() {
+	/*
+		1. read directory
+		2. display content to sdtout
+	*/
+
+	files, err := os.ReadDir(".")
+
+	if err != nil {
+		log.Fatalf("Error reading directory %s", err)
+	}
+
+	for _, file := range files {
+		fmt.Println(file.Name())
+	}
+}
